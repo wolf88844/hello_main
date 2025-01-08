3f9abe2da7776428dd34055702e177ecad1d6475
wolf88844 <wolf88840@163.com>
2025-01-08 15:02:15 +0800

    feat(api): 恢复 hello 世界接口的功能
    - 重新启用了 hello 世界接口的完整功能
    - 引入了 ApplicationState 以访问配置信息
    - 使用 State 提取器来获取应用状态
    - 优化了代码结构和格式

--- cli_app/src/api/handlers/hello.rs
+++ cli_app/src/api/handlers/hello.rs
@@ -1,16 +1,18 @@
-use axum::{extract::State, http::StatusCode};
-pub async fn hello() -> Result<String, StatusCode> {
-    // Ok(format!(
-    //     "\nhello world! Using configuration from {}\n\n",
-    //     state
-    //         .settings
-    //         .load()
-    //         .config_info
-    //         .location
-    //         .clone()
-    //         .unwrap_or("missing config location".to_string())
-    // )
-    // .to_string())
+use std::sync::Arc;
 
-    Ok("hello world!".to_string())
+use axum::{extract::State, http::StatusCode};
+
+use crate::state::ApplicationState;
+pub async fn hello(State(state):State<Arc<ApplicationState>>) -> Result<String, StatusCode> {
+    Ok(format!(
+        "\nhello world! Using configuration from {}\n\n",
+        state
+            .settings
+            .load()
+            .config_info
+            .location
+            .clone()
+            .unwrap_or("missing config location".to_string())
+    )
+    .to_string())
 }
