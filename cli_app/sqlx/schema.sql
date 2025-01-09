3b4ef361f3f97caae9472471f8949aed27c24571
wolf88844 <wolf88840@163.com>
2025-01-09 11:15:23 +0800

    fix: 修改文件名大小写 添加pgsql建表语句

---
+++ cli_app/sqlx/schema.sql
@@ -0,0 +1,43 @@
+#pgSql
+create table users(
+    id serial primary key,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    status int not null default 1,
+    created timestamp default current_timestamp,
+    updated timestamp default current_timestamp,
+    last_login timestamp
+)
+
+create unique index idx_users_username on users(username);
+
+create table posts(
+    id serial primary key,
+    author_id int not null,
+    slug varchar(255) not null,
+    title varchar(255) not null,
+    content text not null,
+    status int not null default 1,
+    created timestamp default current_timestamp,
+    updated timestamp default current_timestamp,
+	foreign key (author_id) references users(id)
+)
+
+create unique index idx_posts_slug on posts(slug);
+
+create or replace function updated_at_column()
+returns trigger as $$
+begin
+    new.updated = current_timestamp;
+    return new;
+end
+$$ language plpgsql;
+
+create trigger updated_at_column
+before update on users
+for each row execute procedure updated_at_column();
+
+create trigger updated_at_column
+before update on posts
+for each row execute procedure updated_at_column();
+
